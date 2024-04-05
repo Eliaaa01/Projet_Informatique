9be226790854ec3bac2ce32b4dad2cf44ac270d4
Eliaaa01 eaucher01@insa-strasbourg.fr <>
2024-04-05 18:27:04 +0200

    j'ai ajouté toutes les classes

---
+++ src/main/java/fr/insa/aucher/projet_informatique2/Projet_Informatique2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.insa.aucher.projet_informatique2;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class Projet_Informatique2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
